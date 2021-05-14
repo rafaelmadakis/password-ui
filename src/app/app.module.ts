00efe95c76c7025f329ec79005d4e9380eface48
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-05-13 22:45:11 -0300

    Tela de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
+import { MatCommonModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HomeComponent } from './views/home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar/';
+import {MatIconModule} from '@angular/material/icon';
+
+import {MatButtonModule} from '@angular/material/button';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    
+    MatToolbarModule,
+    MatIconModule ,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
