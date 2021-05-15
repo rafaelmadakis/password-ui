748db85efd14499a1c04e73cdcf1fa738a1e26c6
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-05-15 18:19:52 -0300

    Requisição da API - inicio

---
+++ src/app/shared/models/pass.model.ts
@@ -0,0 +1,12 @@
+export class Pass{
+
+ /**
+  * Maperar campos da entidade com seus tipos de dados
+  * 
+  * id: string;
+  * sistema: string;
+  * password: string;
+  */
+
+
+}
---
+++ src/app/shared/models/responsePageable.model.ts
@@ -0,0 +1,15 @@
+export class ResponsePageable {
+
+  /**
+   * Mapear os atributos de paginação:
+   * content: any[];
+   * first: boolean;
+   * last: boolean
+   * number: number
+   *   ...
+   *   ...
+   *   ...
+   * totalPages: number
+   * 
+   */
+}
